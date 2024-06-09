275f8d50983eb2e8ac8c52588468253597f91ba2
Sarvagya Kumar <111130903+sarvagyakrcs@users.noreply.github.com>
2024-06-09 23:57:28 +0530

    Update page.tsx

--- src/app/(auth)/password/reset/page.tsx
+++ src/app/(auth)/password/reset/page.tsx
@@ -1,13 +1,15 @@
 import PasswordResetForm from '@/components/PasswordResetForm'
 import PasswordResetVerification from '@/components/PasswordResetVerification'
-import React from 'react'
+import { useSearchParams } from 'next/navigation';
 
 type Props = {}
 
 const PasswordResetPage = (props: Props) => {
     return (
         <div className='h-full w-full flex items-center justify-center'>
-            <PasswordResetVerification/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <PasswordResetVerification/>
+            </Suspense>
         </div>
     )
 }
